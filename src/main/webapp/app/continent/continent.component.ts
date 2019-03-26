fb43a777f28171010b3b8e9670bcd7db7191f52d
udepardin <pardiniraharja@gmail.com>
2019-03-26 10:12:28 +0700

    If user entered some substring of continent (e.g 'xy') and there is no continent contains that substring, show all

--- src/main/webapp/app/continent/continent.component.ts
+++ src/main/webapp/app/continent/continent.component.ts
@@ -39,22 +39,18 @@ export class ContinentComponent implements OnInit {
     }
 
     searchContinent() {
-        if (this.model === '') {
+        if (this.model === '' || this.continents.filter(continent => continent.name === this.model.name).length <= 0) {
             this.continentService.getContinents().subscribe((responses: any[]) => (this.continents = responses));
         } else {
-            if (this.continents.filter(continent => continent.name === this.model.name).length > 0) {
-                this.continentService
-                    .getContinents()
-                    .pipe(
-                        map((continents: any[]) => {
-                            return continents.filter(continent => continent.name === this.model.name);
-                        })
-                    )
-                    .subscribe((responses: any[]) => (this.continents = responses));
-                this.router.navigate(['/continent/' + this.model.name + '/view']);
-            } else {
-                this.continentService.getContinents().subscribe((responses: any[]) => (this.continents = responses));
-            }
+            this.continentService
+                .getContinents()
+                .pipe(
+                    map((continents: any[]) => {
+                        return continents.filter(continent => continent.name === this.model.name);
+                    })
+                )
+                .subscribe((responses: any[]) => (this.continents = responses));
+            this.router.navigate(['/continent/' + this.model.name + '/view']);
         }
     }
 }
