6148efa8c85af854a583576af1e4312b7f9908da
udepardin <pardiniraharja@gmail.com>
2019-04-02 14:44:19 +0700

    made country detail page similar my studies stusy settings

--- src/main/webapp/app/continent/continent-country-detail.component.ts
+++ src/main/webapp/app/continent/continent-country-detail.component.ts
@@ -2,15 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ICountry } from './continent.model';
 import { ContinentService } from './continent.service';
+import { threadId } from 'worker_threads';
+import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-continent-country-detail',
-    templateUrl: './continent-country-detail.component.html'
+    templateUrl: './continent-country-detail.component.html',
+    styleUrls: ['continent-country-detail.scss']
 })
 export class ContinentCountryDetailComponent implements OnInit {
     country: any;
     CountryCode: string;
-    constructor(protected activatedRoute: ActivatedRoute, protected continentService: ContinentService) {}
+    pageTabs: any[] = [];
+    selectedTabId: string;
+
+    constructor(
+        protected activatedRoute: ActivatedRoute,
+        protected continentService: ContinentService,
+        protected alertService: JhiAlertService
+    ) {}
 
     ngOnInit() {
         this.CountryCode = this.activatedRoute.snapshot.params['countrycode'];
@@ -18,6 +28,27 @@ export class ContinentCountryDetailComponent implements OnInit {
         // this.activatedRoute.data.subscribe(({ country }) => {
         //     this.country = country;
         // });
+        this.prepareTabs();
+        this.clickTab('CountryDetail1');
+    }
+
+    prepareTabs() {
+        this.pageTabs = [];
+
+        this.pageTabs.push({
+            name: 'Country Detail 1',
+            id: 'CountryDetail1'
+        });
+
+        this.pageTabs.push({
+            name: 'Country Detail 2',
+            id: 'CountryDetail2'
+        });
+    }
+
+    clickTab(tabId) {
+        this.alertService.clear();
+        this.selectedTabId = tabId;
     }
 
     previousState() {
